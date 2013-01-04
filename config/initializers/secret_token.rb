d662858b45c5e3721afba85a0a95c0b1ab740444
Ryan Grablick <rgrablick@gmail.com>
2013-01-03 22:27:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rygr::Application.config.secret_token = '180c7920b0e407041f37b5d97fb9cd6812f3cadbd999c32b489f6c11afcdc619ccf59d6ffb172aa5d0d5567d0416437c2b5e4a90e9cb13461a9827e241a421f6'
